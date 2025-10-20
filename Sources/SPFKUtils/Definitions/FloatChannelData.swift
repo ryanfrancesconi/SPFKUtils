8c3efb8eb6168bb1a89fd1f396d6ed6a82fc2cfe
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-10-20 11:52:52 -0700

    refactor WaveformData into struct refactor Transport buttons

--- Sources/SPFKUtils/Definitions/FloatChannelData.swift
+++ Sources/SPFKUtils/Definitions/FloatChannelData.swift
@@ -2,3 +2,11 @@ import Foundation
 
 /// 2D array of multiple float arrays
 public typealias FloatChannelData = [[Float]]
+
+/// Initialize a new FloatChannelData. Convenience function.
+public func newFloatChannelData(channelCount: Int, length: Int) -> FloatChannelData {
+    Array(
+        repeating: Array<Float>(zeros: length),
+        count: channelCount
+    )
+}

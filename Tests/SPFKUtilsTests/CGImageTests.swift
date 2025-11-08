e3c18956d0df8cf1a0f1fae482c5caa0913f58e7
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-11-07 17:26:15 -0800

    updates to codable for MetaAudioFileDescription to make it compatible with SwiftData Fixed for BEXT description and umid fields

--- Tests/SPFKUtilsTests/CGImageTests.swift
+++ Tests/SPFKUtilsTests/CGImageTests.swift
@@ -8,16 +8,23 @@ import UniformTypeIdentifiers
 
 class CGImageTests {
     @Test func cgImageDataRoundtrip() async throws {
-        let url = TestBundleResources.shared.cowbell_wav
-        let cgImage = try #require(url.finderIcon?.cgImage)
-        Log.debug(cgImage)
+        let url = TestBundleResources.shared.sharksandwich
 
-        let data = try cgImage.dataRepresentation(utType: .png)
-        Log.debug(data)
-        
-        let newImage = NSImage(data: data)?.cgImage
+        let originalImage = try #require(NSImage(contentsOf: url)?.cgImage)
+        Log.debug(originalImage)
+
+        let data = try #require(originalImage.jpegRepresentation)
+        let newImage = try #require(NSImage(data: data)?.cgImage)
+
         Log.debug(newImage)
 
-        #expect(newImage == cgImage)
+        #expect(newImage.width == originalImage.width)
+        #expect(newImage.height == originalImage.height)
+        #expect(newImage.colorSpace == originalImage.colorSpace)
+        #expect(newImage.utType == originalImage.utType)
+        #expect(newImage.bytesPerRow == originalImage.bytesPerRow)
+        #expect(newImage.bitsPerPixel == originalImage.bitsPerPixel)
+        #expect(newImage.bitsPerComponent == originalImage.bitsPerComponent)
+        #expect(newImage.alphaInfo == originalImage.alphaInfo)
     }
 }

a41ec1db576603022246c9406312281ec725e781
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-11-16 13:31:13 -0800

    large commit and begining full refactor process. Moving listeners back into C

--- Sources/SPFKUtils/Extensions/Foundation/Task.swift
+++ Sources/SPFKUtils/Extensions/Foundation/Task.swift
@@ -4,7 +4,12 @@ import Foundation
 
 extension Task where Success == Never, Failure == Never {
     public static func sleep(seconds: TimeInterval) async throws {
-        let duration = UInt64(seconds * 1000000000)
-        try await Task.sleep(nanoseconds: duration)
+        if #available(macOS 13, *) {
+            try await Task.sleep(for: .seconds(seconds))
+
+        } else {
+            let nanoseconds = UInt64(seconds * 1000000000)
+            try await Task.sleep(nanoseconds: nanoseconds)
+        }
     }
 }

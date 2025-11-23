99d60c558debcc468982c94bb8514cc8eb8bd692
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-11-23 14:05:41 -0800

    refactoring

--- Sources/SPFKUtils/Definitions/Progress/Progress.swift
+++
@@ -1,9 +0,0 @@
-// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/SPFKUtils
-
-import Foundation
-
-/// Indicates a range of 0 ... 1
-public typealias UnitInterval = Double
-
-/// Indicates a range of 0 ... 100
-public typealias Percentage = Double

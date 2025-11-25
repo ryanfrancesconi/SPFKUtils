80a29c64bb0ccc3167d7509f58f84f8017cbefcc
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-11-25 07:12:38 -0800

    Package.swift cleanup

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version: 6.2.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
 import Foundation
+import PackageDescription
 
 private let name: String = "SPFKUtils" // Swift target
 private let dependencyNames: [String] = ["SPFKBase", "SPFKTesting"]
@@ -22,10 +22,10 @@ let remoteDependencies: [RemoteDependency] = [
     .init(package: .package(url: "https://github.com/rnine/Checksum", branch: "master"),
           product: .product(name: "Checksum", package: "Checksum")),
     .init(package: .package(url: "https://github.com/jozefizso/swift-xattr", branch: "main"),
-          product: .product(name: "XAttr", package: "swift-xattr")),
+          product: .product(name: "XAttr", package: "swift-xattr"))
 ]
 
-// MARK: - Reusable Code for a Swift + C package
+// MARK: - Reusable Code for a dual Swift + C package
 
 struct RemoteDependency {
     let package: PackageDescription.Package.Dependency
@@ -41,78 +41,64 @@ private let products: [PackageDescription.Product] = [
 ]
 
 private var packageDependencies: [PackageDescription.Package.Dependency] {
-     let local: [PackageDescription.Package.Dependency] =
+    let local: [PackageDescription.Package.Dependency] =
         dependencyNames.map {
             .package(name: "\($0)", path: "../\($0)") // assumes the package garden is in one folder
         }
 
-        
-     let remote: [PackageDescription.Package.Dependency] =
+    let remote: [PackageDescription.Package.Dependency] =
         dependencyNames.map {
             .package(url: "\(githubBase)/\($0)", branch: dependencyBranch)
         }
-    
-    var value = useLocalDependencies ? local : remote
-    
-    if !remoteDependencies.isEmpty {
-        value.append(contentsOf: remoteDependencies.map { $0.package } )
-    }
-    
-    return value
-}
 
-// is there a Sources/[NAME]/Resources folder?
-private var swiftTargetResources: [PackageDescription.Resource]? {
-    // package folder
-    let root = URL(fileURLWithPath: #file).deletingLastPathComponent()
-    
-    let dir = root.appending(component: "Sources")
-        .appending(component: name)
-        .appending(component: "Resources")
-    
-    let exists = FileManager.default.fileExists(atPath: dir.path)
-    
-    return exists ? [.process("Resources")] : nil
+    var value = useLocalDependencies ? local : remote
+
+    if !remoteDependencies.isEmpty {
+        value.append(contentsOf: remoteDependencies.map { $0.package })
+    }
+
+    return value
 }
 
 private var swiftTargetDependencies: [PackageDescription.Target.Dependency] {
     let names = dependencyNames.filter { $0 != "SPFKTesting" }
-    
+
     var value: [PackageDescription.Target.Dependency] = names.map {
         .byNameItem(name: "\($0)", condition: nil)
     }
-    
+
     value.append(.target(name: nameC))
-    
+
     if !remoteDependencies.isEmpty {
-        value.append(contentsOf: remoteDependencies.map { $0.product } )
+        value.append(contentsOf: remoteDependencies.map { $0.product })
     }
-    
+
     return value
 }
 
 private let swiftTarget: PackageDescription.Target = .target(
     name: name,
     dependencies: swiftTargetDependencies,
-    resources: swiftTargetResources
+    resources: nil
 )
 
 private var testTargetDependencies: [PackageDescription.Target.Dependency] {
     var array: [PackageDescription.Target.Dependency] = [
         .byNameItem(name: name, condition: nil),
-        .byNameItem(name: nameC, condition: nil),
+        .byNameItem(name: nameC, condition: nil)
     ]
 
     if dependencyNames.contains("SPFKTesting") {
         array.append(.byNameItem(name: "SPFKTesting", condition: nil))
     }
-    
+
     return array
 }
 
 private let testTarget: PackageDescription.Target = .testTarget(
     name: nameTests,
-    dependencies: testTargetDependencies
+    dependencies: testTargetDependencies,
+    resources: nil
 )
 
 private var cTargetDependencies: [PackageDescription.Target.Dependency] {
@@ -132,7 +118,6 @@ private let cTarget: PackageDescription.Target = .target(
         .headerSearchPath("include_private")
     ]
 )
-
 
 private let targets: [PackageDescription.Target] = [
     swiftTarget, cTarget, testTarget

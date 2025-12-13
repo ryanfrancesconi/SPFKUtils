8cd9818161dd2bf17631b5477539aba8d31562d4
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-13 08:43:14 -0800

    playlist mode fixes

--- Sources/SPFKUtils/Extensions/AppKit/NSTableView.swift
+++ Sources/SPFKUtils/Extensions/AppKit/NSTableView.swift
@@ -1,65 +1,64 @@
 #if canImport(AppKit) && !targetEnvironment(macCatalyst)
-import AppKit
+    import AppKit
 
-extension NSTableView {
-    public func selectNextRow(byExtendingSelection: Bool = false) throws {
-        var nextRow = min(numberOfRows - 1, selectedRow + 1)
+    extension NSTableView {
+        public func selectNextRow(byExtendingSelection: Bool = false) throws {
+            var nextRow = min(numberOfRows - 1, selectedRow + 1)
 
-        if hiddenRowIndexes.isNotEmpty, hiddenRowIndexes.contains(nextRow) {
-            for i in nextRow ..< numberOfRows {
-                if !hiddenRowIndexes.contains(i) {
-                    nextRow = i
-                    break
+            if hiddenRowIndexes.isNotEmpty, hiddenRowIndexes.contains(nextRow) {
+                for i in nextRow ..< numberOfRows {
+                    if !hiddenRowIndexes.contains(i) {
+                        nextRow = i
+                        break
+                    }
                 }
             }
-        }
 
-        guard nextRow > selectedRow else {
-            throw NSError(description: "This is the last row")
-        }
-
-        let selectRow = IndexSet(integer: nextRow)
-        selectRowIndexes(selectRow, byExtendingSelection: byExtendingSelection)
-        scrollSelectedRowToVisible()
-    }
-
-    public func selectPreviousRow(byExtendingSelection: Bool = false) throws {
-        guard selectedRow - 1 >= 0 else {
-            throw NSError(description: "This is the first row")
-        }
-
-        var nextRow = max(0, selectedRow - 1)
-
-        if hiddenRowIndexes.isNotEmpty, hiddenRowIndexes.contains(nextRow) {
-            var i = nextRow
-            while i >= 0 {
-                if !hiddenRowIndexes.contains(i) {
-                    nextRow = i
-                    break
-                }
-                i -= 1
+            guard nextRow > selectedRow else {
+                throw NSError(description: "This is the last row")
             }
+
+            let selectRow = IndexSet(integer: nextRow)
+            selectRowIndexes(selectRow, byExtendingSelection: byExtendingSelection)
+            scrollSelectedRowToVisible()
         }
 
-        let selectRow = IndexSet(integer: nextRow)
-        selectRowIndexes(selectRow, byExtendingSelection: byExtendingSelection)
-        scrollSelectedRowToVisible()
-    }
+        public func selectPreviousRow(byExtendingSelection: Bool = false) throws {
+            guard selectedRow - 1 >= 0 else {
+                throw NSError(description: "This is the first row")
+            }
 
-    public func scrollSelectedRowToVisible() {
-        scrollRowToVisible(selectedRow)
-    }
+            var nextRow = max(0, selectedRow - 1)
 
-    
-    /// an `IndexSet` containing the indices of all columns in the table view
-    public var allColumnIndexes: IndexSet {
-        var indexSet = IndexSet()
+            if hiddenRowIndexes.isNotEmpty, hiddenRowIndexes.contains(nextRow) {
+                var i = nextRow
+                while i >= 0 {
+                    if !hiddenRowIndexes.contains(i) {
+                        nextRow = i
+                        break
+                    }
+                    i -= 1
+                }
+            }
 
-        for i in 0 ..< self.tableColumns.count {
-            indexSet.insert(i)
+            let selectRow = IndexSet(integer: nextRow)
+            selectRowIndexes(selectRow, byExtendingSelection: byExtendingSelection)
+            scrollSelectedRowToVisible()
         }
 
-        return indexSet
+        public func scrollSelectedRowToVisible() {
+            scrollRowToVisible(selectedRow)
+        }
+
+        /// an `IndexSet` containing the indices of all columns in the table view
+        public var allColumnIndexes: IndexSet {
+            var indexSet = IndexSet()
+
+            for i in 0 ..< tableColumns.count {
+                indexSet.insert(i)
+            }
+
+            return indexSet
+        }
     }
-}
 #endif

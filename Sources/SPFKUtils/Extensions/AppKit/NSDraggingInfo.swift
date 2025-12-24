8fe34fbaa107ac9ef20f9032496cf5217e060bef
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-24 08:21:13 -0800

    fix duplicate files added to table when dragging embedded folders of items

--- Sources/SPFKUtils/Extensions/AppKit/NSDraggingInfo.swift
+++ Sources/SPFKUtils/Extensions/AppKit/NSDraggingInfo.swift
@@ -1,54 +1,55 @@
 #if canImport(AppKit) && !targetEnvironment(macCatalyst)
-import AppKit
+    import AppKit
 
-extension NSDraggingInfo {
-    @MainActor
-    public func toFileURLs() throws -> [URL] {
-        guard let items = draggingPasteboard.pasteboardItems,
-              items.isNotEmpty,
-              let types = draggingPasteboard.types else {
-            throw NSError(description: "pasteboardItems is empty")
-        }
-
-        var urls = [URL]()
-
-        for item in items {
-            guard let type = item.availableType(from: types) else {
-                Log.error("Failed to determine type for", item)
-                continue
+    extension NSDraggingInfo {
+        @MainActor
+        public func toFileURLs() throws -> [URL] {
+            guard let items = draggingPasteboard.pasteboardItems,
+                  items.isNotEmpty,
+                  let types = draggingPasteboard.types
+            else {
+                throw NSError(description: "pasteboardItems is empty")
             }
 
-            guard let url = convertToURL(type: type, item: item) else {
-                Log.error("Failed to parse URL from \(type)")
+            var urls = [URL]()
 
-                continue
+            for item in items {
+                guard let type = item.availableType(from: types) else {
+                    Log.error("Failed to determine type for", item)
+                    continue
+                }
+
+                guard let url = convertToURL(type: type, item: item) else {
+                    Log.error("Failed to parse URL from \(type)")
+
+                    continue
+                }
+
+                urls.append(url)
             }
 
-            urls.append(url)
+            guard urls.isNotEmpty else {
+                throw NSError(description: "No files were found")
+            }
+
+            return urls
         }
 
-        guard urls.isNotEmpty else {
-            throw NSError(description: "No files were found")
-        }
+        private func convertToURL(type: NSPasteboard.PasteboardType, item: NSPasteboardItem) -> URL? {
+            guard type == .fileURL else {
+                return nil
+            }
 
-        return urls
+            guard let stringValue = item.string(forType: type) else {
+                Log.error("failed to convert", type, "to string")
+                return nil
+            }
+
+            guard let url = URL(string: stringValue), url.exists else {
+                return nil
+            }
+
+            return url
+        }
     }
-
-    private func convertToURL(type: NSPasteboard.PasteboardType, item: NSPasteboardItem) -> URL? {
-        guard type == .fileURL else {
-            return nil
-        }
-
-        guard let stringValue = item.string(forType: type) else {
-            Log.error("failed to convert", type, "to string")
-            return nil
-        }
-
-        guard let url = URL(string: stringValue), url.exists else {
-            return nil
-        }
-
-        return url
-    }
-}
 #endif
